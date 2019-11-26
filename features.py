e63abadb1a3a0d6f327e9a119c8b65be72df2f45
Narayan Acharya <narayan.acharya6@gmail.com>
2019-11-26 12:45:13 -0500

    Minor cleanup

--- features.py
+++ features.py
@@ -31,31 +31,27 @@ def get_move_comment(node):
 
 def get_center_control(node):
     center_squares = [chess.E4, chess.E5, chess.D4, chess.D5]
-    white_center = 0
-    black_center = 0
-    board = node.board()
-    for center_square in center_squares:
-        white_center += len(board.attackers(chess.WHITE, center_square))
-        black_center += len(board.attackers(chess.BLACK, center_square))
-
-    return white_center, black_center
+    return get_number_of_attackers(node.board(), center_squares)
 
 
 def get_diagonal_control(node):
     main_diagonal = [chess.A8, chess.B7, chess.C6, chess.D5, chess.E4, chess.F3, chess.G2, chess.H1]
     oppo_diagonal = [chess.A1, chess.B2, chess.C3, chess.D4, chess.E5, chess.F6, chess.G7, chess.H8]
     diagonal_squares = main_diagonal + oppo_diagonal
-    white_diagonal = 0
-    black_diagonal = 0
-    board = node.board()
-    for diag_square in diagonal_squares:
-        white_diagonal += len(board.attackers(chess.WHITE, diag_square))
-        black_diagonal += len(board.attackers(chess.BLACK, diag_square))
-
-    return white_diagonal, black_diagonal
+    return get_number_of_attackers(node.board(), diagonal_squares)
 
 
 def get_engine_eval(engine, depth, time, node):
     board = node.board()
     info = engine.analyse(board, chess.engine.Limit(time=time, depth=depth))
     return str(info.score)
+
+
+def get_number_of_attackers(board, squares):
+    white = 0
+    black = 0
+    for square in squares:
+        white += len(board.attackers(chess.WHITE, square))
+        black += len(board.attackers(chess.BLACK, square))
+
+    return white, black
